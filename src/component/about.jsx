e9bbed4bbe363dce8f81075f5937dcb672431565
Vishal Kora <vishalkora3665@gmail.com>
2023-07-09 20:47:53 +0530

    bug fix

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -19,11 +19,7 @@ function About() {
                         </div>
                         <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
                             <h3 className='title'>Frontend Developer</h3>
-                            <p class="fst-italic">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                magna aliqua.
-                            </p>
-                            <div class="row">
+                            <div class="row mt-4">
                                 <div class="col-lg-6">
                                     <ul>
                                         <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>11 May 1998</span></li>
