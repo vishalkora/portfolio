468249b5d2b4d737ad13ccb443f9c5a2b7cd5aac
Vishal Kora <vishalkora3665@gmail.com>
2023-07-09 20:45:22 +0530

    bug fix

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -28,8 +28,6 @@ function Contact() {
                                 <h3>Social Profiles</h3>
                                 <div class="social-links">
                                     <a href="https://github.com/vishalkora" target='_blank' class="github"><i class="bi bi-github"></i></a>
-                                    <a href="https://medium.com/@Vishalkora" target='_blank' class="medium"><i class="bi bi-medium"></i></a>
-                                    <a href="https://stackoverflow.com/users/18223400/vishal-kora" target='_blank' class="stackOverflow"><i class="bi bi-stack-overflow"></i></a>
                                     <a href="https://www.linkedin.com/in/vishalkora" target='_blank' class="linkedin"><i class="bi bi-linkedin"></i></a>
                                     <a href="https://web.whatsapp.com/send?phone=919510175147&text=hello!" target='_blank' class="whatsapp"><i class="bi bi-whatsapp"></i></a>
                                 </div>
